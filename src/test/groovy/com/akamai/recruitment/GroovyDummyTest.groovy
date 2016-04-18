20b8f4b0650f35bebc3a64ab87da79882f3ce0bd
snowak <snowak@akamai.com>
2016-04-18 15:33:44 +0200

    Project seed

---
+++ src/test/groovy/com/akamai/recruitment/GroovyDummyTest.groovy
@@ -0,0 +1,11 @@
+package com.akamai.recruitment
+
+import spock.lang.Specification
+
+class GroovyDummyTest extends Specification {
+
+    def "should always pass"() {
+        expect:
+        true
+    }
+}
---
+++ src/test/java/com/akamai/recruitment/DummyTest.java
@@ -0,0 +1,12 @@
+package com.akamai.recruitment;
+
+import org.assertj.core.api.Assertions;
+import org.junit.Test;
+
+public class DummyTest {
+
+    @Test
+    public void shouldAlwaysPass() throws Exception {
+        Assertions.assertThat(true).isTrue();
+    }
+}
